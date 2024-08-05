b89d30cb7a7f525abac79a07fe64ba810e1546cd
Emily Ott <161410357+Em-Ott@users.noreply.github.com>
2024-08-05 13:56:51 -0700

    Add files via upload

---
+++ scenes/main.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+#Tetris TO DO:
+#Create sprites for all of the blocks, and assign them to a number in an array may need to do something similar to snake segments?
+#Add timer for block'w movement
+#Add random block selecyion + new block once old block gone
+#Make it so blocks disappear and score goes up when you have a solid line
+#make it possible to rotate pieces and bring them down faster
+#rotate 90 degrees
+#Piece preview
+
+#Background is roughly 0-480px x 0-1500px y it should fit 10 squares so each movement is -+ 48
+#For my pieces I can use either 2D collision logic or keep track of the individual squares 
+#I will be going with collision logic as I believe it will be beneficial to easier piece interactions
+
